6b353fbab126cc721184cbcd731d17e6c616fd91
Kaouthar Kouaz <kkouaz@e2r9p3.1337.ma>
2023-10-23 01:50:53 +0100

    jjj

--- Ex05/Harl.cpp
+++ Ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: kkouaz <kkouaz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 21:53:14 by kkouaz            #+#    #+#             */
-/*   Updated: 2023/10/22 23:16:27 by kkouaz           ###   ########.fr       */
+/*   Updated: 2023/10/23 01:36:35 by kkouaz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,4 @@ void  Harl :: warning(void)
 void  Harl :: error(void)
 {
     std :: cout << "ERROR message" << std :: endl;
-}
-
-void Harl :: complain(std :: string level)
-{
-   myFunct()->level;
 }
